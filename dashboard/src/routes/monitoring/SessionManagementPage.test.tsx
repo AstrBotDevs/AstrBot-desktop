e61218c07348dfa34de681ffa674012d810d4093
Fridemn <702625325@qq.com>
2026-07-31 15:25:31 +0800

    style(dashboard): format session management test

--- dashboard/src/routes/monitoring/SessionManagementPage.test.tsx
+++ dashboard/src/routes/monitoring/SessionManagementPage.test.tsx
@@ -71,9 +71,7 @@ describe('SessionManagementPage', () => {
     await screen.findByText('user-1');
     const checkboxes = screen.getAllByRole('checkbox');
     await user.click(checkboxes[1]);
-    await user.click(
-      screen.getByRole('button', { name: 'features.session-management.batchOperations.llmStatus' }),
-    );
+    await user.click(screen.getByRole('button', { name: 'features.session-management.batchOperations.llmStatus' }));
     await user.click(screen.getByRole('option', { name: 'features.session-management.status.enabled' }));
     await user.click(screen.getByRole('button', { name: 'features.session-management.batchOperations.apply' }));
 
